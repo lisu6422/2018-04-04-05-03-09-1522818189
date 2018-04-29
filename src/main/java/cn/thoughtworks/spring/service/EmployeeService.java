8bad0daa137205ea2ee0288d05a52b61a17c06ba
likly <569553290@qq.com>
2018-04-29 10:24:35 +0800

    添加 Service

---
+++ src/main/java/cn/thoughtworks/spring/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package cn.thoughtworks.spring.service;
+
+import cn.thoughtworks.spring.domain.Employee;
+
+import java.util.List;
+
+/**
+ * @author likly
+ * @version 1.0
+ * @date 2018-04-29 10:23
+ * @since 1.0
+ */
+public interface EmployeeService {
+
+    List<Employee> selectAll();
+
+
+}
