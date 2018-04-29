6220f7770bc926e40ce7b6efbb49269904b3de77
likly <569553290@qq.com>
2018-04-29 10:26:12 +0800

    添加Controller

---
+++ src/main/java/cn/thoughtworks/spring/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package cn.thoughtworks.spring.controller;
+
+import cn.thoughtworks.spring.domain.Employee;
+import cn.thoughtworks.spring.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author likly
+ * @version 1.0
+ * @date 2018-04-29 10:24
+ * @since 1.0
+ */
+@RestController
+public class EmployeeController {
+
+    @Resource
+    private EmployeeService employeeService;
+
+    @RequestMapping("/employees")
+    public List<Employee> selectAll() {
+        return employeeService.selectAll();
+    }
+}
