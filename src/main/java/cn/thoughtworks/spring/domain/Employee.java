3cb85417028d0683918daac046849c938457c5d8
likly <569553290@qq.com>
2018-04-29 10:13:57 +0800

    添加 Employee 对象

---
+++ src/main/java/cn/thoughtworks/spring/domain/Employee.java
@@ -0,0 +1,60 @@
+package cn.thoughtworks.spring.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author likly
+ * @version 1.0
+ * @date 2018-04-29 10:11
+ * @since 1.0
+ */
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = -235812500524704751L;
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
